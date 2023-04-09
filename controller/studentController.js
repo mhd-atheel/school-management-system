52790178840ec27aaa750d7b3348381618abf78d
mhd-atheel <aathilmazz1234@gmail.com>
2023-04-10 00:01:35 +0530

    uncomment

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,13 +1,13 @@
 const Student = require('../models/studentModel')
 
 
-// app.post('/users', async (req, res) => {
-//     try {
-//       const student = new Student(req.body);
-//       await student.save();
-//       res.status(201).json(student);
-//     } catch (error) {
-//       res.status(400).json({ error: error.message });
-//     }
-//   });
+app.post('/users', async (req, res) => {
+    try {
+      const student = new Student(req.body);
+      await student.save();
+      res.status(201).json(student);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
