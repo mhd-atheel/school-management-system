045e3d9615a2d58b0e9eef2dab9fb16fec8197f0
mhd-atheel <aathilmazz1234@gmail.com>
2023-04-14 00:01:54 +0530

    Create UPDATE AND DELETE API

--- controller/studentController.js
+++ controller/studentController.js
@@ -2,26 +2,24 @@ const Student = require('../models/studentModel');
 
 
 
-// app.post('/users', async (req, res) => {
-//     try {
-//       const student = new Student(req.body);
-//       await student.save();
-//       res.status(201).json(student);
-//     } catch (error) {
-//       res.status(400).json({ error: error.message });
-//     }
-//   });
-
-
+// Create Student 
+// http://localhost:3000/student/add-student/
+// POST REQUEST
 const addStudent = async(req,res)=>{
 
   try {
-
-    const student = new Student(req.body);
-    await student.save();
-    res.status(201).json({"data":student});
+    const {email} = req.body;
+    
+    const oldUser = await Student.findOne({email})
+    if(oldUser){
+      res.status(409).send("student Email Already Exist".toUpperCase);
+    }
+    else{
+      const student = new Student(req.body);
+      await student.save();
+      res.status(201).json({"data":student});
+    }
   }
-  
   catch (error) {
 
     res.status(400).json({ error: error.message });
@@ -29,10 +27,13 @@ const addStudent = async(req,res)=>{
   }
 }
 
-
+// Get All Student
+//http://localhost:3000/student/get-student/
+//GET REQUEST
 const getAllStudents = async(req,res)=>{
 
   try {
+
     const user = await Student.find()
     res.status(201).json(user);
 
@@ -45,22 +46,71 @@ const getAllStudents = async(req,res)=>{
   }
 }
 
-
-
+// Get Student by ID 
+//http://localhost:3000/student/get-student-by-id/6438372ff37f64f3e8c75ab5
+//GET REQUEST
 const getStudentById = async(req,res)=>{
 
     try {
+
       const id = req.params.id
       const student = await Student.findById(id)
       res.status(201).json(student);
+
     } catch (error) {
+
       res.status(400).json({ error: error.message });
+
     }
+
+}
+
+
+// Update Student by ID 
+//http://localhost:3000/student/update-student-by-id/6438372ff37f64f3e8c75ab5
+//PUT REQUEST
+const updateStudent = async(req,res)=>{
+
+    try {
+
+      const id = req.params.id
+      const body = req.body
+      const student = await Student.findByIdAndUpdate(id, body, { new: true });
+      res.status(201).json(student);
+
+    } catch (error) {
+
+      res.status(400).json({ error: error.message });
+
+    }
+
+}
+
+// Delet Student by ID 
+//http://localhost:3000/student/delete-student-by-id/6438372ff37f64f3e8c75ab5
+//DELETE REQUEST
+const deleteStudent = async(req,res)=>{
+
+   try {
+    
+    const id = req.params.id
+      await Student.findByIdAndDelete(id)
+      res.status(201).send(">>>>>>> ID OF "+ id +" DELETED SUCCESSFULLY");
+    
+
+   } catch (error) {
+
+    res.status(400).json({ error: error.message });
+
+   }
+
 }
 
 
 module.exports = {
   addStudent,
   getAllStudents,
-  getStudentById
+  getStudentById,
+  updateStudent,
+  deleteStudent
 }
--- models/studentModel.js
+++ models/studentModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 
 const studentSchema =  mongoose.Schema({
 
   biodata :
     {
-      first_name: {
+    first_name: {
         type: String,
         required: true
     },
@@ -49,7 +50,31 @@ const studentSchema =  mongoose.Schema({
       type: Number,
       required: false,
   },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: false,
+    validate: {
+      validator: (value) => {
+        // Use a regular expression to validate the email formats
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: 'Invalid email format',
     },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        // Check if the password is at least 8 characters long and contains at least one special character
+        return /^(?=.*?[!@#$%^&*()_+-=])(?=.*?[A-Za-z]).{8,}$/.test(value);
+      },
+      message: 'Password must be at least 8 characters long and contain at least one special character',
+    },
+  },
+
   leaves: [{
     reason:{
       type: String,
@@ -76,14 +101,38 @@ const studentSchema =  mongoose.Schema({
     required: false
   },
   complaints : [{
-    type: String,
-    required: false
+    reason:{
+      type: String,
+      required: true
+    },
+    date:{
+      type: String,
+      required: true
+    },
   }]
     
     
 },
   { timestamps: true }
 );
+
+studentSchema.pre('save', async function (next) {
+  try {
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash the password with the salt
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+
+    // Replace the plaintext password with the hashed password
+    this.password = hashedPassword;
+
+    // Move on to the next middleware
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = mongoose.model('Students', studentSchema);
 module.exports = User;
